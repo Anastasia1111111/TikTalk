b2f2353a4e766c9829b10a50ca527098da3abc36
Плакса Анастасия Олеговна <nastassia.plaksa@softclub.by>
2025-01-30 12:11:14 +0300

    add pipe and using input()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
 import { ProfileService } from './data/services/profile.service';
-import { CommonModule } from '@angular/common';
+import { Profile } from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCardComponent, CommonModule],
+  imports: [ProfileCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'tik-talk';
-  profiles: any = [];
+  profiles: Profile[] = [];
   constructor(private profileService: ProfileService) {
     this.profileService.getTestAccounts().subscribe((val) => {
       this.profiles = val;
--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Profile } from '../interfaces/profile.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +9,6 @@ export class ProfileService {
   constructor(private http: HttpClient) {}
   baseApiUrl = 'https://icherniakov.ru/yt-course/';
   getTestAccounts() {
-    return this.http.get(`${this.baseApiUrl}account/test_accounts`);
+    return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`);
   }
 }
